74c3d4abfb5e58d43dc5801f953895fa5b9beae7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-03 15:21:12 +0300

    App + Model + Sandbox

---
+++ models/Thing.js
@@ -0,0 +1,18 @@
+class Thing{
+  static _tableName = 'things';
+  static _client;
+
+  static async create(){}
+
+  static async findByPk(){}
+
+  static async findAll(){}
+
+  static async updateByPk(){}
+
+  static async deleteByPk(){}
+
+}
+
+
+module.exports = Thing
---
+++ sandbox/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+const path = require("path");
+const client = require("pg/lib/native/client");
+
+console.log(__dirname);
+
+console.log(__filename);
+
+const currentFileName = path.basename(__filename);
+///home/user/MY-TASKS/SERVER/SERVER-MVC-CRUD-ROMAN/sandbox/index.js
+
+const db = {};
+
+const fileNames = fs.readdirSync(__dirname);
+console.log(fileNames);
+
+const filtredArray = fileNames.filter(
+  (currentFile) => /.js$/.test(currentFile) && currentFile !== currentFileName
+);
+console.log(filtredArray);
+
+filtredArray.forEach((currentFile)=>{
+  const absPathToFile =  path.resolve(__dirname, currentFile)
+  const Model = require(absPathToFile);
+  Model.__client = client;
+  db[Model._tableName] = Model;
+});
